87b6096c7073f935758a59d182fb5b401663f9c7
jyoti <jyotivish0806@gmail.com>
2025-06-19 16:23:56 +0530

    we recieve email and password

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def about():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return "the email is {} and the password is {}".format(email, password)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

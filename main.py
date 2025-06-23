3d3ed08e7aac5cf789558ebe6e4ae5383de20a5b
jyoti <jyotivish0806@gmail.com>
2025-06-23 16:44:35 +0530

    add functionality , if user has login then only show home page otherwise show login page only

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+import mysql.connector  # pyright: ignore[reportMissingImports]
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="4321",
+    database="mydb"
+)
+cursor = conn.cursor()
 
 @app.route('/')
 def login():
@@ -12,13 +23,23 @@ def about():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect('/')
 
-@app.route('/login_validation', methods=['POST'])
+@app.route('/login_validation',  methods=['POST'])
 def login_validation():
     email = request.form.get('email')
     password = request.form.get('password')
-    return "the email is {} and the password is {}".format(email, password)
+    cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}' AND `password` LIKE '{}'""".format(email,password))
+    users = cursor.fetchall()
+    print(users)
+    if len(users)>0:
+        session['user_id'] = users[0][0]
+        return redirect('/home')
+    else:
+        return redirect('/')
 
 
 
